d804b0b6893c210ebd4afc0e50d493ebbf6e1c38
Chittaranjan Chandwani <89841114+chittaranjanchandwani@users.noreply.github.com>
2024-05-22 10:35:35 +0530

    Add files via upload

---
+++ blogs/src/main/java/com/example/blogs/payloads/PostDto.java
@@ -0,0 +1,27 @@
+package com.example.blogs.payloads;
+
+import com.example.blogs.entities.Category;
+import com.example.blogs.entities.Comment;
+import com.example.blogs.entities.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostDto {
+
+    private Integer postId;
+    private String title;
+    private String content;
+    private String imageName;
+    private Date addDate;
+    private CategoryDto category;
+    private UserDto user;
+    private Set<CommentDto> comments=new HashSet<>();
+}
